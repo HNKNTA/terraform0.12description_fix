f989fa06e73110c182286cb9b87905e350e6b07f
HNKNTA <hnknta@gmail.com>
2020-04-10 16:42:50 +0300

    init script

---
+++ terraform0.12description_fix.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+
+def fix_variable_definition(variable_first_line, commented_description):
+    proper_description = re.sub(r"#\s*", "",
+                                commented_description)
+    proper_description = re.sub(r"(\"\$\{)(.+)(\}\")", r"\g<2>",
+                                proper_description)
+    return """{}\n  description = <<EOD
+    {}EOD""".format(variable_first_line, proper_description)
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r+") as f:
+        read_file = f.read()
+
+        found_list = re.findall(r"(#.+?)(\b(variable|output)\b\s+?\".+?\".+?\s*?\{)", read_file, re.DOTALL)
+        print(found_list)
+        for _tuple in found_list:
+            read_file = read_file.replace(_tuple[0], '')
+            read_file = read_file.replace(
+                _tuple[1],
+                fix_variable_definition(_tuple[1], _tuple[0])
+            )
+
+        f.seek(0)
+        f.write(read_file)
+        f.truncate()
